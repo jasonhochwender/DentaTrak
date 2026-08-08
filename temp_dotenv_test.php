ad046adaf9cff7f0adbff1c75d84f5240d933ced
Jason H <jason.hochwender@dentatrack.com>
2026-08-08 15:14:19 -0400

    Remove temporary dotenv test file

--- temp_dotenv_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-Dotenv\Dotenv::createImmutable(__DIR__)->load();
-
-$var = 'ENCRYPTION_KEY';
-echo "getenv($var): ";
-var_dump(getenv($var));
-echo "\$_ENV[$var]: ";
-var_dump($_ENV[$var] ?? null);
